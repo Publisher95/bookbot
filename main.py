db8cc670b7938d54f053e5f33824753a23e36211
pub <mikeyxboxsilver@yahoo.com>
2025-01-10 21:18:34 -0700

    finished both counts

--- main.py
+++ main.py
@@ -1,9 +1,27 @@
-    
 def main():
+    letterDict = {}
+    wordCount = 0
     with open("books/frankenstein.txt") as f:
         file_contents = f.read()
 
-    print(file_contents)
+    for words in file_contents.split():
+        wordCount += 1
+
+    for character in file_contents.lower():
+        if character not in letterDict:
+            letterDict[character] = 1
+        else:
+            letterDict[character] += 1
+        
+
+    
+
+
+
+
+    printf("There are {wordCount} words in this book.")
+
+
 
 
 main()
